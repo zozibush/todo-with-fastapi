09a2fa09cf69ad456679678fb6d21a92fb62b1ea
zozibush <zozibush88@gmail.com>
2024-04-03 00:27:00 +0900

    Add update_todo endpoint to todo_router

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Path
-from model import Todo
+from model import Todo, TodoItem
 
 todo_router = APIRouter()
 
@@ -20,3 +20,12 @@ async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to 
         if todo.id == todo_id:
             return {"todo": todo}
     return {"message": "Todo not supplied ID doesn't exist"}
+
+@todo_router.put("/todo/{todo_id}")
+async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The ID of the todo to be updated.")) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            todo.item = todo_data.item
+            return {"message":"Todo updated successfully"}
+    return {"message": "Todo with supplied ID doesn't exist"}
+
