bf87182f54af1504144750efd68da2608d684b0b
zozibush <zozibush88@gmail.com>
2024-04-03 00:19:19 +0900

    Add endpoint for single todo

--- todo.py
+++ todo.py
@@ -13,3 +13,10 @@ async def add_todo(todo: Todo) -> dict:
 @todo_router.get("/todo")
 async def get_todo() -> dict:
     return {"todo_list": todo_list}
+
+@todo_router.delete("/todo/{todo_id}")
+async def get_single_todo(todo_id: int) -> dict:
+    for todo in todo_list:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return {"message": "Todo not supplied ID doesn't exist"}
