00e5d8a46c3cb5b5dcb73dedba8e1506a7763104
zozibush <zozibush88@gmail.com>
2024-04-03 00:26:55 +0900

    Add TodoItem model with example schema

--- model.py
+++ model.py
@@ -3,3 +3,11 @@ from pydantic import BaseModel
 class Todo(BaseModel):
     id: int
     item: str
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {"item": "Read the next chapter of the book."}
+        }
