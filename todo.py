ec2bee236e78382e1ddc9c421f9030d8926afb7f
zozibush <zozibush88@gmail.com>
2024-04-03 00:29:15 +0900

    Add delete_todo and delete_all_todo routes

--- todo.py
+++ todo.py
@@ -29,3 +29,16 @@ async def update_todo(todo_data: TodoItem, todo_id: int = Path(..., title="The I
             return {"message":"Todo updated successfully"}
     return {"message": "Todo with supplied ID doesn't exist"}
 
+@todo_router.delete("/todo/{todo_id}")
+async def delete_todo(todo_id: int) -> dict:
+    for index in range(len(todo_list)):
+        todo = todo_list[index]
+        if todo.id == todo_id:
+            todo_list.pop(index)
+            return {"message":"Todo deleted successfully"}
+    return {"message":"Todo with supplied ID doesn't exist"}
+
+@todo_router.delete("/todo")
+async def delete_all_todo() -> dict:
+    todo_list.clear()
+    return {"message":"Todos deleted successfully"}
