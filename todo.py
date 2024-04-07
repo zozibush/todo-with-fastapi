e38da9d13e7cbca5c675c518b0738ff5e6e82786
zozibush <zozibush88@gmail.com>
2024-04-07 20:40:39 +0900

    Add TodoItems model to get_todo endpoint

--- todo.py
+++ todo.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Path
-from model import Todo, TodoItem
+from model import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
 
@@ -10,7 +10,7 @@ async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
     return {"message": "Todo added successfully"}
 
-@todo_router.get("/todo")
+@todo_router.get("/todo", response_model=TodoItems)
 async def get_todo() -> dict:
     return {"todo_list": todo_list}
 
