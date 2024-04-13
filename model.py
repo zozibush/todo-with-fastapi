591a9fa471cd0d26ecf989e2cd15f8597c182073
zozibush <zozibush88@gmail.com>
2024-04-13 22:39:45 +0900

    Refactor TodoItem model to include an optional id field and a classmethod to create instances from form data in model.py

--- model.py
+++ model.py
@@ -1,13 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Optional
 from typing import List
+from fastapi import Form
 
 class Todo(BaseModel):
     id: int
     item: str
 
 class TodoItem(BaseModel):
+    id: Optional[int]
     item: str
 
+    @classmethod
+    def as_form(cls, item: str = Form(...)):
+        return cls(item=item)
     class Config:
         schema_extra = {
             "example": {"item": "Read the next chapter of the book."}
