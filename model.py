fb97975017431778deb2a38f41f5c154a46e06a7
zozibush <zozibush88@gmail.com>
2024-04-13 23:05:40 +0900

    Fix import statements in model.py for better readability

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel, Optional
-from typing import List
+from pydantic import BaseModel
+from typing import List, Optional
 from fastapi import Form
 
 class Todo(BaseModel):
